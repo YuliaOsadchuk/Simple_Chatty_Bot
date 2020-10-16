1c85ae0b853c2015816124b5aa49fc002f5d71e6
Yulia Osadchuk <yulia.osadchuk@gmail.com>
2020-10-16 23:03:12 +0300

    add problem Snail

---
+++ Problems/Snail/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int h = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        boolean finish = false;
+        int heightDay = 0;
+        int day = 0;
+
+        while (!finish) {
+            heightDay += a;
+            day++;
+            if (heightDay >= h) {
+                finish = true;
+            } else {
+                heightDay -= b;
+            }
+        }
+
+        System.out.println(day);
+    }
+}
